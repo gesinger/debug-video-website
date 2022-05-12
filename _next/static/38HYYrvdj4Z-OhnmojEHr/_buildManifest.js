88868638fb6241385d9e5ec3665eb232fe707622
Garrett Singer <gesinger@gmail.com>
2022-05-12 09:38:49 -0400

    Deploy Next.js to gh-pages

---
+++ _next/static/38HYYrvdj4Z-OhnmojEHr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/490-d932cfd66fd1a675.js","static/chunks/pages/index-ed9215342cfd84b6.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
